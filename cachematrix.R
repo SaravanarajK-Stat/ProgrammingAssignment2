b33dca730c44645c799b2895e7a00d85e6f5e371
SARAVANARAJ K <73639940+SaravanarajK-Stat@users.noreply.github.com>
2020-12-14 19:15:56 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
   set <- function(y) {
     x <<- y
@@ -55,7 +55,3 @@ cacheSolve <- function(x, ...) {
   x$setinv(inv)
   inv
 }
-
-m <- matrix(rnorm(16),4,4)
-m1 <- makeCacheMatrix(m)
-cacheSolve(m1)
